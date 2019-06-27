10f2154cfb9afd1fb0b90345786b64d1696be989
Adam Foryś <adam@appsilon.com>
2019-06-27 07:47:33 +0200

    Init Auth0 API Client (#1)
    * Init Auth0 API Client
    
    * Fix descriptions, dependencies and add examples.

---
+++ R/package.R
@@ -0,0 +1,103 @@
+
+#' Aut0 API Client
+#'
+#' Minimal wrapper to access Auth0 API.
+#'
+#' @docType package
+#' @name auth0api
+NULL
+
+
+#' Query the Auth0 Managment API
+#'
+#' This is an extremely minimal client. You need to know the Auth0 API
+#' to query Auth0 endpoint.
+#' \itemize{
+#'   \item Tries to substitute each listed parameter into
+#'     \code{endpoint}, using the \code{{param}} notation.
+#'     Replaced \code{{param}} will be parsed with \code{glue} package using provided \code{...} parameters.
+#'   \item If a GET request is chosen (the default), then adds
+#'     all other listed parameters as query parameters.
+#'   \item If not a GET request, then sends the other parameters
+#'     in the request body as JSON.
+#'   \item The response is converted to an R list using
+#'     \code{jsonlite::fromJSON}.
+#' }
+#'
+#' @param endpoint Auth0 API endpoint. Must be one of the following forms:
+#'
+#'    \itemize{
+#'      \item "METHOD path", e.g. "GET /api/v2/users/{id}"
+#'      \item "path", e.g. "/api/v2/users/{id}".
+#'      \item "METHOD url", e.g. "GET https://auth0-org-domain.auth0.com/api/v2/users/{id}"
+#'      \item "url", e.g. "https://auth0-org-domain.aut0.com/api/v2/users/{id}".
+#'    }
+#'
+#'    If the method is not supplied, \code{.method} parameter is used (\code{GET} by default).
+#' @param ... Name-value pairs giving API parameters. Will be matched
+#'   into \code{url} placeholders, sent as query parameters in \code{GET}
+#'   requests, and in the JSON body of \code{POST} requests.
+#' @param .auth_key Authentication token. Sourced by default with \link{api_token}
+#' @param .domain Auth0 API domain (default: \link{api_domain}).
+#'   Used if \code{endpoint}  contains a path only.
+#' @param .method HTTP method to use if not explicitly supplied in the
+#'    \code{endpoint}.
+#' @param .headers Named character vector of header field values
+#'   (except \code{Authorization}, which is handled via
+#'   \code{.token}). This can be used to override or augment the
+#'   defaults, which are as follows: the \code{Accept} field set up
+#'   to \code{"application/json"} by default and the
+#'   \code{User-Agent} field \code{"https://github.com/Appsilon/auth0-api"} - default.
+#'   This can be used, e.g. to provide a custom headers for special needs.
+#' @return Response from the Auth0 API as \code{aut0_api} object
+#' @examples
+#' \dontrun{
+#' ## Get user data from Auth0 Endpoint
+#' auth0("GET /api/v2/users/{id}", id = "auth0|userid")
+#' auth0("GET /api/v2/users/{id}", id = "auth0|userid", field = "nickname", include_fields = "false")
+#' }
+#' @export
+auth0 <- function(endpoint, ..., .domain = NULL, .auth_key = api_token(), .method = "GET", .headers = NULL) {
+  if(missing(.domain) && !grepl("^http", endpoint)) .domain <- api_domain()
+
+  req <- build_request(
+    endpoint,
+    token = .auth_key,
+    api_url = .domain,
+    method = .method,
+    headers = .headers,
+    params = list(...)
+  )
+
+  resp <- make_request(req)
+  if (http_type(resp) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+
+  parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = FALSE)
+
+  if (http_error(resp)) {
+    stop(
+      sprintf(
+        "Auth0 API request failed [%s]\n%s\n<%s>",
+        status_code(resp),
+        parsed$message,
+        parsed$documentation_url
+      ),
+      call. = FALSE
+    )
+  }
+
+  structure(
+    list(
+      content = parsed,
+      endpoint = endpoint,
+      response = resp
+    ),
+    class = "aut0_api"
+  )
+}
+
+aut0_api.print <- function(x, ...) {
+  print(x$content, ...)
+}
---
+++ R/user.R
@@ -0,0 +1,37 @@
+#' Get a user endpoint
+#'
+#'  The endpoint: \url{https://auth0.com/docs/api/management/v2#!/Users/get_users_by_id} can be used to retrieve user details given the \code{user_id}.
+#'
+#' @param user_id The id of the user to retrieve
+#' @param fields A comma separated list of fields to include or exclude (depending on include_fields) from the result.
+#' If left empty then all fields are retrieved.
+#' @param include_fields "true" if the fields specified are to be included in the result, "false" otherwise. Default is "true".
+#' @param ... other \link{auth0} settings.
+#' @return Response from the Auth0 API as \code{aut0_api} object.
+#' @examples
+#' \dontrun{
+#'   get_user("google-oauth2|123123123213123123123")
+#' }
+#' @export
+get_user <- function(user_id, fields = NULL, include_fields = NULL, ...) {
+  auth0("GET /api/v2/users/{user_id}", user_id = user_id, fields = fields, include_fields = include_fields, ...)
+}
+
+#' Update a user
+#'
+#'  The endpoint: \url{https://auth0.com/docs/api/management/v2#!/Users/patch_users_by_id} can be used to update user details given the \code{user_id}.
+#'
+#' @param user_id The user_id of the user to update.
+#' @param ... Parameters that should be updated in user data.
+#' @return Answer from the Auth0 API as \code{aut0_api} object.
+#' @examples
+#' \dontrun{
+#' ## Update user metadata
+#' update_user("google-oauth2|123213123123123123123", user_metadata = list(key = "value"))
+#' ## Update user email_verified and user_metadata
+#' update_user("google-oauth2|123213123123123123123", user_metadata = list(key = "value"), email_verified = TRUE)
+#' }
+#' @export
+update_user <- function(user_id, ...) {
+  auth0("PATCH /api/v2/users/{user_id}", user_id = user_id, ...)
+}
